7b3e83f11ab6086594bb52b918882e28de033d0e
yusuke.fujinaga <i7love1myhome9@gmai.com>
2020-01-10 02:41:36 +0000

    for heroku7

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,8 +7,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   
   if Rails.env.development?
     storage :file
-  # elsif Rails.env.test?
-  #   storage :file
+  elsif Rails.env.test?
+    storage :file
   else
     storage :fog
   end
