e6d7ef3f137f5644e9ce92d9f9c09d8220091bef
yusuke.fujinaga <i7love1myhome9@gmai.com>
2020-01-10 04:18:21 +0000

    for heroku 10

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,7 +20,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
   
   # storage :file
-  storage :fog
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
