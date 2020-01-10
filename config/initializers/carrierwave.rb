8b066293aa4fdb648aa2721a3fa93a52b3f55f24
yusuke.fujinaga <i7love1myhome9@gmai.com>
2020-01-10 05:44:42 +0000

    for heroku 15

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,22 +2,20 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
+if Rails.env.production?
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'winepostsbaket'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION'],
-      path_style: true
-    }
-  else
-    config.storage :file
-    config.enable_processing = false if Rails.env.test?
-  end
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+
+    provider: 'AWS',
+    region:  ENV['S3_REGION'],
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+
+  }
+  config.fog_directory  = ENV['S3_BUCKET']
+end
 end
  
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
