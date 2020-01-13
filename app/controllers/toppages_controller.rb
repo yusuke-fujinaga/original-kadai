204944bfe110986047771dcb67c29aa5662b0436
yusuke.fujinaga <i7love1myhome9@gmai.com>
2020-01-13 09:56:14 +0000

    add css last

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -25,19 +25,19 @@ class ToppagesController < ApplicationController
 			
 			if wine_type == nil 
 			 @wineposts = Winepost.all.page(params[:page]).per(9)
-			 @text = '投稿一覧'
+			 @text = 'Wineposts'
 			elsif wine_type == '赤ワイン'
 			 @wineposts = Winepost.where(wine_type: '赤ワイン').page(params[:page]).per(9)
-			 @text = '赤ワイン一覧'
+			 @text = 'Red wine posts'
 			elsif wine_type == '白ワイン'
 			 @wineposts = Winepost.where(wine_type: '白ワイン').page(params[:page]).per(9)
-			 @text = '白ワイン一覧'
+			 @text = 'White wine posts'
 			elsif wine_type == 'オレンジワイン'
 			 @wineposts = Winepost.where(wine_type: 'オレンジワイン').page(params[:page]).per(9) 
-			 @text = 'オレンジワイン一覧'
+			 @text = 'Orange wine posts'
 			elsif wine_type == 'ロゼワイン'
 			 @wineposts = Winepost.where(wine_type: 'ロゼワイン').page(params[:page]).per(9) 
-			 @text = 'オレンジワイン一覧'
+			 @text = 'Rose wine posts'
 			end
 		end	
 	end
